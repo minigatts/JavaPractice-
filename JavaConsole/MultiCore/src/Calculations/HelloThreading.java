ed60aace90e0828b04371bdfdf9296366a11ba6c
Chantz <chantz.drake@gmail.com>
2016-11-14 10:41:14 -0600

    A few threading example codes.  V. Simple stuff.

---
+++ JavaConsole/MultiCore/src/Calculations/HelloThreading.java
@@ -0,0 +1,11 @@
+public class HelloThreading implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new Thread(new HelloThreading())).start();
+    }
+
+}
---
+++ JavaConsole/MultiCore/src/Calculations/SleepMessages.java
@@ -0,0 +1,28 @@
+/*
+ * This is an efficient means of making processor time available to the other 
+ * threads of an application or other applications that might be running on a 
+ * computer system.
+*/
+
+public class SleepMessages {
+    public static void main(String args[])
+        throws InterruptedException 
+        {
+            String importantInfo[] = 
+            {
+                "Mares eat oats.",
+                "Does eat oats.",
+                "Little lambs eat ivy.",
+                "A kid will eat ivy too.",
+                "Wouldn't you?"
+            };
+
+            for (int i = 0;  i < importantInfo.length; i++) 
+            {
+                //Pause for 4 seconds
+                Thread.sleep(4000);
+                //Print a message
+                System.out.println(importantInfo[i]);
+            }
+        }
+}
