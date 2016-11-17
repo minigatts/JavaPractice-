91674306c8fb7b69967be2c6022211542625395f
Chantz <chantz.drake@gmail.com>
2016-11-17 09:19:14 -0600

    Still messing with idea of multithreading.  No luck yet...

--- JavaConsole/MultiCore/src/threadMerge/arraySplit.java
+++ JavaConsole/MultiCore/src/threadMerge/arraySplit.java
@@ -1,6 +1,8 @@
 package threadMerge;
 
+// <editor-fold defaultstate="collapsed" desc="Imports">
 import java.math.*;
+// </editor-fold>
 
 // The purpose of this code is to determine how many cores are available, and 
 // then split a dataset (array) evenly across all cores.
@@ -9,8 +11,28 @@ public class arraySplit implements Runnable
 {
        
     public static void main(String args[])          
+    {       
+       // Generate 2D array with rows = # cores on machine.
+       int[][] array = genArrays();
+       
+       // Send each row to a seperate thread.        
+       for(int i = 0 ; i < array.length ; i++ )
+       { 
+           int[] subarray = new int[array[0].length];
+           
+           for(int j = 0 ; i < array[0].length ; j++)
+           {
+               subarray[j]=array[i][j];
+           }
+                    
+       }
+       
+       
+    }
+    
+    public static int[][] genArrays()
     {
-        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1};
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2};
         System.out.println("Your input array contains " + array.length + " elements.");      
         for(int i = 0; i<array.length; i++) System.out.print(array[i] + " ");
         
@@ -32,48 +54,58 @@ public class arraySplit implements Runnable
         // array cannot be evenly divided across the cores, the 'odd' element
         // will be added to the last sub-array.
         if( (double) array.length % cores != 0)
+        {
             System.out.println("This can't be divided evenly, so most arrays "
-                    + "will have " + (int) Math.round(L) + " elements, but the last array "
-                    + "will have " + (array.length - (int) Math.round(L) * ( cores -1 ) ) + " elements.\n");
+                    + "will have " + (int) Math.ceil(L) + " elements, but the last array "
+                    + "will have " + (array.length - (int) Math.ceil(L) * ( cores -1 ) ) + " elements.\n");
+        
+            System.out.println("This will ocasionally result in the last array\n"
+                    + "being underpopulated, possibly having only one element.\n"
+                    + "In later versions I will code to optimize the division\n"
+                    + "of elements better.\n");
+        }
+             
         
         // Reorganize the array into 2 dimensional array.
         // Array will have rows = cores, columns equal to division.
         // Final row may need to be populated with null elements.
         
-        int[][] splitdata = new int [(int) Math.round(L)][cores];
-        int elements = (int) Math.round(L);  // Max number of elements per row.
+        int[][] splitdata = new int [(int) Math.ceil(L)][cores];
+        int elements = (int) Math.ceil(L);  // Max number of elements per row.
         int count = 0; 
         
+        // Fill 2D Array to prepare for split.
         for(int j = 0; j < cores ; j++ ) // Iterate through rows.
         {
-            System.out.print("\nRow " + (j+1) + " : ");
+            // System.out.print("\nRow " + (j+1) + " : ");
             for(int i = 0; i < elements ; i++ )  // Iterate through columns.
-            {
-                splitdata[i][j] = array[count];
-                System.out.print(splitdata[i][j]+ " ");
+            {                
+                if( count < array.length) splitdata[i][j] = array[count];
+                else{splitdata[i][j] = -999; }  // Flag null entries with -999.
+               
                 count++;
-            }
-            
+            }            
         }
         
         // Print each 'subset' seperately. 
         for( int j = 0 ; j < cores ; j++ )
         {
-            System.out.print("\nList " + (j+1) + " : ");
+            System.out.print("Group " + (j+1) + " : ");
             for ( int i = 0 ; i < elements ; i++)
             {
                 System.out.print(splitdata[i][j] + " ");
             }
+            System.out.print("\n");
         }
         
+        return splitdata;
         
     }
     
-    public void run() 
+    public void run(int[] array) 
     {
          //new Thread(new arraySplit()).start();
         
-        System.out.println("Hello from a thread!");        
-    }
-     
+        System.out.println("New Thread Started");        
+    }     
 }
